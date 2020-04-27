e89d13a51d14f6c2dd9e91dfbc7b6778c8a9872a
Samyak Shah <samyak2083@gmail.com>
2020-04-27 11:03:19 +0530

    Add files via upload

---
+++ variables/p2.py
@@ -0,0 +1,62 @@
+x = 5 #int type vaicle
+y = "Samyak" #string type varible
+print(x)
+print(y)
+
+#####################################
+
+name = "sam" #in double quote
+# is the same as
+name = 'sam' #in single quote
+
+#####################################
+
+#Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#Illegal variable names:
+2myvar = "John"  #ERROR
+my-var = "John"  #ERROR
+my var = "John"  #ERROR
+
+######################################
+
+a, b, c = "blue", "green", "red"  #assign 3 values in 3 varibles.
+print(a)
+print(b)
+print(c)
+
+aa,bb,cc = "Red" #divide the word into 3 parts
+print(aa)
+print(bb)
+print(cc)
+
+xx=yy=zz = "blue" #print the same value for 3 varibles
+print(xx)
+print(yy)
+print(zz)
+
+#########################################
+
+str = "Amazing"
+print("Python is " + str) #print the value of valible after print statement by using +
+
+str2 = "Python is "
+str3 = "Amazing"
+str4 = str2 + str3
+print(str4) #print the concate value of 2 string
+
+#########################################
+
+no1 = 10
+no2 = 15
+print(no1 + no2) #this will add value of two int vaible.
+
+str5 = 'hello'
+no3 = 5
+print(str5 + no3)#You can not add one str and on int value. #ERROR
