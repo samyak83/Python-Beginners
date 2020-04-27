f41b0cc479196f406b8e53e7209947635a769b0b
Samyak Shah <samyak2083@gmail.com>
2020-04-27 22:56:22 +0530

    Add files via upload

---
+++ casting/p5.py
@@ -0,0 +1,22 @@
+i1 = int(1)   # x will be 1
+i2 = int(2.8) # y will be 2
+i3 = int("3") # z will be 3
+print(i1)
+print(i2)
+print(i3)
+
+f1 = float(1)     # x will be 1.0
+f2 = float(2.8)   # y will be 2.8
+f3 = float("3")   # z will be 3.0
+f4 = float("4.2") # w will be 4.2
+print(f1)
+print(f2)
+print(f3)
+print(f4)
+
+s1 = str("s1") # x will be 's1'
+s2 = str(2)    # y will be '2'
+s3 = str(3.0)  # z will be '3.0'
+print(s1)
+print(s2)
+print(s3)
---
+++ numbers/p4.py
@@ -0,0 +1,37 @@
+'''There are three numeric types in Python:
+1) int
+2) float
+3) complex'''
+
+###################################
+
+i1 = 1    # int
+f1 = 2.8  # float
+c1 = 1j   # complex
+
+print(type(i1))
+print(type(f1))
+print(type(c1))
+
+###################################
+
+x = 1 # int
+y = 2.8 # float
+z = 1j # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
