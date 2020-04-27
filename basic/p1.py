08f78960e1e756ef3947953b8ce28217ef2c3d99
Samyak Shah <samyak2083@gmail.com>
2020-04-27 10:19:22 +0530

    Add files via upload

--- basic/p1.py
+++ basic/p1.py
@@ -1,10 +1,26 @@
 #commnet in python is given by # symbol
 '''multiple line comment is given by
-line 1
-line 2'''
+line 1 of comment
+line 2 of comment'''
 
 
-#simple print statement
+#simple print statement usnig single quotes
 print('Hello Samyak !')
 
+#simple print statement usnig double quotes
 print("Hi ! Samyak ")
+
+#two argument
+print("Hello", "how are you?")
+
+#double argument seprated by ---
+print("Hello", "how are you?", sep="---")
+
+#single quote in print statement.
+print('Here\'s a single-quote escape')
+
+#double quote in print statement.
+print("This is an \"escape\" of a double-quote")
+
+#putting \n before sentence will print statement in new line
+print("Hi,\nMy name is Jhon,\nand i am 20 years old.")
